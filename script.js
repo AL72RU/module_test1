e2b63164da0e94e1a5c522a4f73bcb34b6a44b35
AL72RU <gshurik88@gmail.com>
2022-06-07 16:05:28 +0500

    Update

---
+++ script.js
@@ -0,0 +1,7 @@
+let modal = document.querySelector('dialog');
+document.querySelector('#open').onclick = function() {
+    modal.showModal();
+}
+document.querySelector('#close').onclick = function() {
+    modal.close();
+}
